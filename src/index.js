4abbe78ee75e81dc3beade4ba907a3ed8ea9d21e
Nikita Zhavoronkov <larkgronick@gmail.com>
2020-03-20 11:20:54 +0300

    feat: add brackets task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let compareConfig =  bracketsConfig.map((item) => item.join(''));
+for (let i = 0; i < compareConfig.length; i++) {
+    if(str.includes(compareConfig[i])){
+        str = str.replace(compareConfig[i], "");
+        i =-1
+    }
+}
+return (str) ? false : true;
+   
 }
